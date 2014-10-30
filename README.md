6b06e579cd16f0e656023c83f16c306a09a37b51
Veronika Bitkina <veronika@zisel.ca>
2014-10-30 13:30:02 -0700

    generating movie model

---
+++ README.md
@@ -0,0 +1,3 @@
+## Rotten Mangoes
+
+
---
+++ db/migrate/20141030202441_create_movies.rb
@@ -0,0 +1,15 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :director
+      t.string :runtime_in_minutes
+      t.string :integer
+      t.text :description
+      t.string :poster_image_url
+      t.datetime :release_date
+
+      t.timestamps
+    end
+  end
+end
