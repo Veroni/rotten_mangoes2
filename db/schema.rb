7b71f56698ed40547960a7e2c521e4c7655486a4
Veronika Bitkina <veronika@zisel.ca>
2014-10-30 17:50:47 -0700

    generating user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030202441) do
+ActiveRecord::Schema.define(version: 20141031001543) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20141030202441) do
     t.text     "description"
     t.string   "poster_image_url"
     t.datetime "release_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
