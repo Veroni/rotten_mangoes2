5b9d5d93d9c2438c63114ebd740ffcf80f03b9f7
Veronika Bitkina <veronika@zisel.ca>
2014-10-30 20:13:03 -0700

    generating review model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ActiveRecord::Base
+  has_many :reviews
+
   validates :title, presence: true
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
