1e425c4272a62f20256cdcac1d517e671ced8e73
Veronika Bitkina <veronika@zisel.ca>
2014-10-30 23:02:35 -0700

    generating reviews controller and reviews/new view

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.build
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.build(review_params) # the same like @review = Review.new(movie_id: @movie.id)
+    @review.user_id = @current_user.id
+
+    if @review.save
+      redirect_to @movie, notice: "Review created successfully!"
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def review_params
+    params.require(:review).permit(:text, :rating_out_of_ten)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
